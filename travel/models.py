f29858ae2c71d94d481ce7f85e17d3da6c8d61ff
raju7011 <rajudhakal7011gmail.com>
2021-09-19 00:11:05 +0545

    models fro inclusion for packages

--- travel/models.py
+++ travel/models.py
@@ -36,4 +36,12 @@ class AllCategory(models.Model):
     def __str__(self):
         return self.title
 
+
+#MODELS FOR INCLUSION
+class Inclusion(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
     
+    def __str__(self):
+        return self.title
