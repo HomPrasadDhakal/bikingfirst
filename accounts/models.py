fcfd6b6ddb05c8e21c288d2ce483afd75642242a
raju7011 <rajudhakal7011gmail.com>
2021-09-18 15:01:31 +0545

    custom models for user and it's manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from accounts.manager import Usermanger
 
-# Create your models here.
+class user(AbstractUser):
+    username = None
+    last_login = None
+    email = models.EmailField(verbose_name='email', unique=True)
+    date_joined = models.DateTimeField(verbose_name="Add joined date", auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    mobile = models.CharField(max_length=20)
+
+    objects = Usermanger()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name","last_name",]
--- bikingfirst/settings.py
+++ bikingfirst/settings.py
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+AUTH_USER_MODEL  = 'accounts.User'
